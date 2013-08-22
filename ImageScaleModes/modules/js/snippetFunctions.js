6b851c28b32f4cd4dfdc0869f7bf79a85804b9a7
unknown <KH1502@KH1502.kitspl.com>
2013-08-22 17:30:07 +0530

    app id changed,forms modified

--- ImageScaleModes/modules/js/snippetFunctions.js
+++ ImageScaleModes/modules/js/snippetFunctions.js
@@ -1,21 +1,41 @@
-
+/*****************************************************************
+*	Name    : FrmImgCrop
+*	Author  : Kony
+*	Purpose : To navigate to 'FrmImgCrop' from the frmOptions form for native platform.
+******************************************************************/
 function navigateToFrmImgCrop(){
 	frmImgCrop.show();
 }
-
+/*****************************************************************
+*	Name    : FrmImgFitToDimensions
+*	Author  : Kony
+*	Purpose : To navigate to 'FrmImgFitToDimensions' from the frmOptions form for native platform.
+******************************************************************/
 function navigateToFrmImgFitToDimensions(){
 	frmImgFitToDimensions.show();
 }
-
+/*****************************************************************
+*	Name    : FrmImgMaintainAspectRation
+*	Author  : Kony
+*	Purpose : To navigate to 'FrmImgMaintainAspectRation' from the frmOptions form for native platform.
+******************************************************************/
 function navigateToFrmImgMaintainAspectRation(){
 	frmImgMaintainAspectRatio.show();
 }
-
+/*****************************************************************
+*	Name    : FrmOptions
+*	Author  : Kony
+*	Purpose : To navigate to 'FrmOptions' from the spaiphone_frmHome form for SPA iphone platform.
+******************************************************************/
 function navigateToFrmOptions(){
 	frmOptions.show();
 }
 
-
+/*****************************************************************
+*	Name    : URLBasedImage
+*	Author  : Kony
+*	Purpose : To navigate to 'URLBasedImage' from the frmOptions form for native platform.
+******************************************************************/
 function navigateToURLBasedImage(){
 	frmURLBasedImage.show();
 }
